fa1dd094e026516edcb76c4fa9a11cc00abba101
raisework <ilha.dev.br@gmail.com>
2021-03-24 15:51:49 -0300

    Create date builder function to work with logic to display the proper results

--- main.js
+++ main.js
@@ -40,3 +40,37 @@ function displayResults(weather) {
     weather.main.temp_max
   )}°c`;
 }
+
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date} ${month} - ${year}`;
+}
