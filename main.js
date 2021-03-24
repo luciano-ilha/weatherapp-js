f9bfc3126238f4ca8657c3c6896b47e122ef6446
raisework <ilha.dev.br@gmail.com>
2021-03-24 15:47:19 -0300

    Create event for pressed enter key

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const api = {
   key: "506cbac2b3ba4e29e7a6542e49fa22bb",
   base: "https://api.openweathermap.org/data/2.5/",
 };
+
+const searchBox = document.querySelector(".topbox-search");
+searchBox.addEventListener("keypress", setQuery);
+
+function setQuery(event) {
+  if (event.keyCode == 13) {
+    getResults(searchBox.value);
+  }
+}
